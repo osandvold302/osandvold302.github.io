2188caddc857a5153de0d62940281274f7d8643b
Olivia Sandvold <ollie302@gmail.com>
2019-12-30 00:40:37 -0600

    Add short activities content

---
+++ src/components/activites.vue
@@ -0,0 +1,25 @@
+<template>
+  <div b-container class="container-activities">
+    <b-row>
+      <b-col class="col-text text-left" offset="1">
+        <h2>Current Activities</h2>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'activities',
+  components: {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+.container-activities {
+  background-image: linear-gradient(to bottom right, #b5d0eb, #93b1cf);
+}
+
+</style>
